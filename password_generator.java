cecba783c0b4fbe499178c047805a104513ab8eb
UDAY G <124630227+oxBinaryBrain@users.noreply.github.com>
2024-02-25 12:47:12 +0530

    java version

---
+++ password_generator.java
@@ -0,0 +1,27 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String LOWERCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_+=";
+
+    public static void main(String[] args) {
+        int length = 12; // Default password length
+        String password = generatePassword(length);
+        System.out.println("Generated Password: " + password);
+    }
+
+    public static String generatePassword(int length) {
+        String allCharacters = LOWERCASE_CHARACTERS + UPPERCASE_CHARACTERS + DIGITS + SPECIAL_CHARACTERS;
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(allCharacters.length());
+            password.append(allCharacters.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
+}
